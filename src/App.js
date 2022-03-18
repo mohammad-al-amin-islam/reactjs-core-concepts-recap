3da59fc85ac021d71b0d06aa5b06c1d1f71ab620
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-18 17:59:24 +0600

    load data practice added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
+      <LoadData></LoadData>
       <District name="Bogura" famousFood="Doi"></District>
       <District name="Cumilla" famousFood="Rosmalai"></District>
       <District name="Chapai" famousFood="Mango"></District>
     </div>
   );
 }
+
+function LoadData() {
+  const [posts, setPost] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPost(data))
+  }, [])
+  return (
+    <div>
+      <h3>Total Post:{posts.length}</h3>
+      {
+        posts.map(post => <Post title={post.title} body={post.body} key={post.id}></Post>)
+      }
+    </div>
+  )
+}
+
+function Post(props) {
+  return (
+    <div style={{ backgroundColor: 'lightblue', margin: '20px', padding: '20px' }}>
+      <h3>{props.title}</h3>
+      <p>{props.body}</p>
+    </div>
+  )
+}
+
+
+
 const districtStyle = {
   backgroundColor: 'gray',
   margin: '20px',
