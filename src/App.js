cc0465ccbde6613819cbd224b7cc896890c9d7ee
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-18 17:41:46 +0600

    event handler practice added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District name="Bogura" famousFood="Doi"></District>
+      <District name="Cumilla" famousFood="Rosmalai"></District>
+      <District name="Chapai" famousFood="Mango"></District>
     </div>
   );
+}
+const districtStyle = {
+  backgroundColor: 'gray',
+  margin: '20px',
+  padding: '20px',
+  borderRadius: '20px'
+}
+function District(props) {
+  const [ratings, setRatings] = useState(1);
+  const giveRatings = () => setRatings(ratings * 2);
+
+  return (
+    <div style={districtStyle}>
+      <h1>Name:{props.name}</h1>
+      <p>Famous food:{props.famousFood}</p>
+      <h4>Ratings:{ratings}</h4>
+      <button onClick={giveRatings}>Give Ratings</button>
+    </div>
+  )
 }
 
 export default App;
